5cc3962ace1268551de9607a9fa0fb8fdbcf5bc0
Mohammadjafarlo <116568263+Mohammadjafarlo@users.noreply.github.com>
2023-09-19 11:48:13 +0330

    1

---
+++ bazybartar/sakht.js
@@ -0,0 +1,13 @@
+function modal(){
+    // header news
+    var Header = document.getElementById('Header').value;
+    document.getElementById('Headerja').innerHTML= Header;
+
+    // img news
+    var img = document.getElementById('img').value;
+    document.getElementById('imgja').src = img;
+
+    // img news
+    var tozih = document.getElementById('tozih').value;
+    document.getElementById('tozihja').src = tozih;
+}
